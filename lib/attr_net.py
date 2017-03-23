86e9fa1ea3c72c2436a2e6bda11b6f5f4a3cfd8a
aquairus <18650013469@163.com>
2017-03-23 20:34:10 +0800

    integration

--- lib/attr_net.py
+++ lib/attr_net.py
@@ -22,7 +22,7 @@ threshold = np.ones(db.num_attr) * 0.5
 
 def get_attr_net(start=0,end=3):
     net_dir="/data/my_rap_network"
-    caffemodel=net_dir+"/data/snapshots/VGG_S_MLL_RAP/0/"+"attr{0}_{1}".format(start,end)+"RAP/vgg_s_mll_rap_iter_100.caffemodel"
+    caffemodel=net_dir+"/data/snapshots/VGG_S_MLL_RAP/0/"+"attr{0}_{1}".format(start,end)+"/RAP/vgg_s_mll_rap_iter_100.caffemodel"
     prototxt=net_dir+"/models/VGG_S_MLL_RAP/train_net_dir/"+""+"test_net_{0}_{1}.prototxt".format(start,end)
 
     attr_net = caffe.Net(prototxt, caffemodel, caffe.TEST)
