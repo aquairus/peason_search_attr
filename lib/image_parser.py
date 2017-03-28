9c9619640fafaa7a08aeb62fa452c946e425edbc
aquairus <18650013469@163.com>
2017-03-28 23:53:22 +0800

    push

--- lib/image_parser.py
+++ lib/image_parser.py
@@ -4,7 +4,7 @@ from lib.obj_detetor import *
 
 attrs=[[0,1]]
 # [1,4],[4,7]]
-all_nets=attr_net.get_all_nets(attrs)
+all_nets=lib.attr_net.get_all_nets(attrs)
 
 
 def parse_image(image_key):
@@ -31,7 +31,7 @@ def parse_one_pedestrian(img):
         # if attr[i]>0 or "Female"in db.attr_eng[i][0][0]:
         #     img_info['attr'].append("{0}  ------ {1}:            \
         #      {2}\n".format(db.attr_eng[i][0][0],db.attr_ch[i][0][0].encode('utf-8'),attr[i]))
-    img_info['layout']=peason_layout.parse_layout(img)
+    img_info['layout']=lib.peason_layout.parse_layout(img)
     return img_info
 
 def get_all_attrs(img):
@@ -39,7 +39,7 @@ def get_all_attrs(img):
     for attr in attrs:
         start=attr[0]
         single_attr_net=all_nets[start]
-        attr, _, score, _ = attr_net.recognize_attr(single_attr_net, img, attr_net.db.attr_group, attr_net.threshold)
+        attr, _, score, _ = lib.attr_net.recognize_attr(single_attr_net, img, lib.attr_net.db.attr_group, lib.attr_net.threshold)
         for i in range(len(attr)):
-            attrs_info[attr_net.db.attr_eng[i+start][0][0]]=attr[i]
+            attrs_info[lib.attr_net.db.attr_eng[i+start][0][0]]=attr[i]
     return attrs_info
