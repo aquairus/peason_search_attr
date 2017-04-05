637dfe1a1b0ecd7fd276def8af00dda089a2d47e
aquairus <18650013469@163.com>
2017-04-05 23:54:29 +0800

    or

--- build_index.py
+++ build_index.py
@@ -24,6 +24,7 @@ if __name__ == '__main__':
         rval , frame = vc.read()
     else:
         rval = False
+
     try:
         es.indices.delete(index='peason_video' )
     except BaseException,e:
@@ -43,6 +44,7 @@ if __name__ == '__main__':
             peason.pop("attr")
             res = es.index(index="peason_video", doc_type='peason', body=peason)
             print res
+        cv2.imwrite('static/img/'+str(index) + '.jpg',frame)    
         print index
         index += 1
         rval, frame = vc.read()
