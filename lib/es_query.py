acbeaae7e2ede1fb26f51de6c4d7664e41873c18
aquairus <18650013469@163.com>
2017-04-05 22:28:53 +0800

    or

--- lib/es_query.py
+++ lib/es_query.py
@@ -16,5 +16,5 @@ def get_esquery(keys,gender):
     new_query=query_template.copy()
     for key in keys:
         new_query['query']['bool']['must'][key]="1"
-    new_query['query']['bool']['filter']['term']["Female"]=str(genter)
+    new_query['query']['bool']['filter']['term']["Female"]=str(gender)
     return new_query
