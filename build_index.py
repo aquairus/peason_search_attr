2bc5bf1593954a50c00e6de9db40c32e5bd7bb76
aquairus <18650013469@163.com>
2017-04-06 01:35:43 +0800

    or

--- build_index.py
+++ build_index.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
             res = es.index(index="peason_video", doc_type='peason', body=peason)
             print res
         cv2.imwrite('static/img/'+str(idx) + '.jpg',draw_im)
-        print index
+        print idx
         idx += 1
         rval, frame = vc.read()
         # cv2.imwrite('result/'+str(index) + '.jpg',frame)
