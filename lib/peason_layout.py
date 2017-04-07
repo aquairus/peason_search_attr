83339fddd5b25961058c3695dc24b9361497799c
aquairus <18650013469@163.com>
2017-04-08 02:48:00 +0800

    orz

--- lib/peason_layout.py
+++ lib/peason_layout.py
@@ -51,7 +51,7 @@ def parse_layout(im):
     scores, boxes = fast_rcnn.test.im_detect(net, im)
 
 
-    CONF_THRESH = 0.65
+    CONF_THRESH = 0.5
     NMS_THRESH = 0.3
     results={}
     for cls_ind, cls in enumerate(CLASSES[1:]):
