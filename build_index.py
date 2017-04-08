6fd25a024b8bd68b46cfce4199013c2d185ae29c
aquairus <18650013469@163.com>
2017-04-08 14:26:28 +0800

    orz

--- build_index.py
+++ build_index.py
@@ -36,25 +36,24 @@ if __name__ == '__main__':
     #while rval:
     for i in range(120):
         if idx<45 and idx>30:
-            rval, frame = vc.read()
-            idx += 1
-            continue
-        draw_im,pedestrian_attr=parse_frame(frame)
+            pass
+        else:
+            draw_im,pedestrian_attr=parse_frame(frame)
 
-        for peason in pedestrian_attr:
-            peason['time']=idx
-            peason['tag']=''
-            for k,v in peason['attr'].items():
-                k=k.replace("-",'')
-                peason[k]=v
-                peason['tag']+=k+" "
-            peason.pop("attr")
-            #if check_peason(peason):
-            res = es.index(index="peason_video", doc_type='peason', body=peason)
-            print res
-        cv2.imwrite('static/img/'+str(idx) + '.jpg',draw_im)
-        print idx
-        idx += 1
-        rval, frame = vc.read()
+            for peason in pedestrian_attr:
+                peason['time']=idx
+                peason['tag']=''
+                for k,v in peason['attr'].items():
+                    k=k.replace("-",'')
+                    peason[k]=v
+                    peason['tag']+=k+" "
+                peason.pop("attr")
+                #if check_peason(peason):
+                res = es.index(index="peason_video", doc_type='peason', body=peason)
+                print res
+            cv2.imwrite('static/img/'+str(idx) + '.jpg',draw_im)
+            print idx
+            idx += 1
+            rval, frame = vc.read()
         # cv2.imwrite('result/'+str(index) + '.jpg',frame)
     ##app.run(host='0.0.0.0', port=8888)
