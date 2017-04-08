1cc03c6133a9a990a00d3b2fc08ea1d83cc70425
aquairus <18650013469@163.com>
2017-04-08 16:26:56 +0800

    orz

--- lib/obj_detetor.py
+++ lib/obj_detetor.py
@@ -17,10 +17,10 @@ imagePath="/Users/apple/desktop/2.jpg"
 def get_peason_bbox(image):
 
     # (rects, weights) = hog.detectMultiScale(image,hitThreshold=0.6, winStride=(2,4),padding=(12, 12), scale=1.08)
-    (rects, weights) = hog.detectMultiScale(image,hitThreshold=0.6, winStride=(4, 4),padding=(8, 8), scale=1.05)
+    (rects, weights) = hog.detectMultiScale(image,hitThreshold=0.4, winStride=(3, 3),padding=(16, 16), scale=1.05)
     rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
     print (weights)
-
+#    (rects, weights) = hog.detectMultiScale(image,hitThreshold=0.6, winStride=(4, 4),padding=(8, 8), scale=1.05)
     pick = non_max_suppression(rects, probs=None, overlapThresh=0.9)
     print (pick)
     for p in pick:
